7c4e85099b433a91e0c465685fd14a976c3171a6
ataurwd <ataurrahman24707@gmail.com>
2025-02-11 18:40:06 +0600

    first commit

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,96 @@
+import { Disclosure } from '@headlessui/react';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import Drawer from "./Drawer";
+import Drawerdata from "./Drawerdata";
+import Signdialog from "./Signdialog";
+import Registerdialog from "./Registerdialog";
+
+interface NavigationItem {
+    name: string;
+    href: string;
+    current: boolean;
+}
+
+const navigation: NavigationItem[] = [
+    { name: 'Home', href: '#/', current: true },
+    { name: 'Courses', href: '#courses', current: false },
+    { name: 'Mentor', href: '#mentor', current: false },
+    { name: 'Group', href: '/', current: false },
+    { name: 'Testimonial', href: '#testimonial', current: false },
+];
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+}
+
+const CustomLink = ({ href, onClick, children }: { href: string; onClick: () => void; children: React.ReactNode }) => {
+    return (
+        <Link href={href} passHref>
+            <span
+                onClick={onClick}
+                className="px-3 py-4 text-lg font-normal"
+            >
+                {children}
+            </span>
+        </Link>
+    );
+};
+
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const [currentLink, setCurrentLink] = useState('/');
+
+    const handleLinkClick = (href: string) => {
+        setCurrentLink(href);
+    };
+
+    return (
+        <Disclosure as="nav" className="navbar">
+            <>
+                <div className="mx-auto max-w-7xl px-6 py-4 lg:px-8">
+                    <div className="relative flex h-12 md:h-20 items-center justify-between">
+                        <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
+
+                            {/* LOGO */}
+
+                            <div className="flex flex-shrink-0 items-center">
+                                <h1 className="block h-12 w-40 lg:hidden">Learning</h1>
+                                <h1 className="hidden h-full w-full lg:block text-3xl font-bold">Learning</h1>
+                            </div>
+
+                            {/* LINKS */}
+
+                            <div className="hidden lg:block m-auto">
+                                <div className="flex space-x-4">
+                                    {navigation.map((item) => (
+                                        <CustomLink
+                                            key={item.name}
+                                            href={item.href}
+                                            onClick={() => handleLinkClick(item.href)}
+                                        >
+                                            <span
+                                                className={classNames(
+                                                    item.href === currentLink ? 'underline-links' : 'text-slategray',
+                                                    'px-3 py-4 text-lg font-normal opacity-75 hover:opacity-100'
+                                                )}
+                                                aria-current={item.href ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </span>
+                                        </CustomLink>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        </Disclosure>
+    );
+};
+
+export default Navbar;
